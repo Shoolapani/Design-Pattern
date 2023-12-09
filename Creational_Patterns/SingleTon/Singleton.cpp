ac141da063f14a1008accecd83e3c66c269c5494
Satyam 3. Kumar (Nokia) <satyam.3.kumar@nokia.com>
2023-12-09 22:39:03 +0530

    Added SingleTon

---
+++ Creational_Patterns/SingleTon/Singleton.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+std::mutex m1;
+
+class SingleTon
+{
+public:
+    ~SingleTon()
+    {
+        // use mutex lock
+        std::unique_lock<mutex> ul(m1);
+
+        if (instanceSingleTon != nullptr)
+        {
+            cout << "Des....." << endl;
+            delete instanceSingleTon;
+            instanceSingleTon = nullptr;
+        }
+        // use mutex unlock
+    }
+
+    // copy constructor
+    SingleTon(const SingleTon &) = delete;
+
+    // copy assignment operator
+    SingleTon &operator=(SingleTon const &) = delete;
+
+    void set(int val);
+    void get()
+    {
+        cout << this->val << endl;
+    }
+    static SingleTon *getInstance()
+    {
+        // use mutex lock
+        if (instanceSingleTon == nullptr)
+        {
+            instanceSingleTon = new (std::nothrow) SingleTon();
+        }
+        return instanceSingleTon;
+    }
+
+private:
+    static SingleTon *instanceSingleTon;
+    SingleTon() : val(0){};
+    int val;
+};
+
+SingleTon *SingleTon::instanceSingleTon = nullptr;
+
+void SingleTon::set(int val)
+{
+    this->val = val;
+}
+
+int main()
+{
+    // use eager instantiation for multi threaded as mutex is costly sometimes.
+    SingleTon *singleTon = SingleTon::getInstance();
+    SingleTon *singleTon2 = singleTon;
+
+    singleTon->set(20);
+    // singleTon->get();
+    singleTon2->set(50);
+    singleTon2->get();
+    singleTon->get();
+
+    // now use n - thread after this
+
+    return 0;
+}
